ea146cbf12b8b148a587b01d122fa5750b3e5bc1
axiomiety <axiomiety@gmail.com>
2012-09-09 07:14:21 +0100

    level 0

---
+++ level0.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+'''
+References:
+http://docs.python.org/library/socket.html
+http://docs.python.org/library/struct.html
+'''
+
+import socket
+import struct
+
+hostname='vortex.labs.overthewire.org'
+port=5842
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((hostname, port))
+
+data = list()
+for i in xrange(4): # 4 integers in host byte order, which we know is x86 (little endian)
+	data.append(struct.unpack('<I', s.recv(4))[0])
+
+# the sum of our 4 ints need to overflow into an int and not a long
+# as the sender expects an int back (trial and error...)
+s.send(struct.pack('<I', sum(data) % 2**32))
+print s.recv(2048) # arbitrary length, but should be enough
+s.close()
